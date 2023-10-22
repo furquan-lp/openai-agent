fd953ce4369b38f84bf3548d25a5e7fcb320df1c
Syed F Ahmad <furquan@protonmail.com>
2023-10-23 02:36:39 +0530

    Added the search function

--- autogen-agent/content-gen-agent.py
+++ autogen-agent/content-gen-agent.py
@@ -17,3 +17,19 @@ from dotenv import load_dotenv
 load_dotenv()
 config_list = config_list_from_json(env_or_file='OAI_CONFIG_LIST')
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+# research function
+def search(query):
+    url = 'https://google.serper.dev/search'
+
+    payload = json.dumps({
+        'q': query
+    })
+    headers = {
+        'X-API-KEY': '584babe795cdececcc761e68443bb663a8a3dad7',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request('`POST', url, headers=headers, data=payload)
+    return response.json()
