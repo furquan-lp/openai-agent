8f78d78b0e81e69f62c3e683356f586bd1e1ae93
Syed F Ahmad <furquan@protonmail.com>
2023-10-22 22:43:52 +0530

    Added the scraping function

--- research-agent/app.py
+++ research-agent/app.py
@@ -22,6 +22,7 @@ load_dotenv()
 browserless_api_key = os.getenv('AGENT_BROWSERLESS_API')
 serper_api_key = os.getenv('AGENT_SERP_API')
 
+# Search Google through Serper and return the results
 def google_search(query):
     url = 'https://google.serper.dev/search'
 
@@ -39,4 +40,30 @@ def google_search(query):
     print(response.text)
     return response.text
 
-google_search('What is the speed of light?')
+# Scrape the website URL and summarize content based on the given objective
+def scrape_website(objective: str, url: str):
+    print('Scraping...')
+
+    headers = {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'url': url
+    }
+
+    data_json = json.dumps(data)
+    # Send the request to Browserless
+    post_url = f'https://chrome.browserless.io/content?token={browserless_api_key}'
+    response = requests.post(post_url, headers=headers, data=data_json)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser') # Extracts the text from the HTML
+        text = soup.get_text()
+        print('SCRAPED CONTENT:', text)
+        return text
+    else:
+        print(f'HTTP request failed with status code {response.status_code}')
+    
+scrape_website("Syed F Ahmad's GitHub", 'https://github.com/furquan-lp')
