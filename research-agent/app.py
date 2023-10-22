7b84c5cba28d5f0893c6c2654a24ffa770eb903f
Syed F Ahmad <furquan@protonmail.com>
2023-10-22 22:53:10 +0530

    Added the summary function

--- research-agent/app.py
+++ research-agent/app.py
@@ -65,5 +65,29 @@ def scrape_website(objective: str, url: str):
         return text
     else:
         print(f'HTTP request failed with status code {response.status_code}')
+
+def summary(objective, content):
+    llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo-16k-0613')
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
+
+    docs = text_splitter.create_documents([content])
+    map_prompt = """
+    Write a summary of the following text for {objective}:
+    "{text}"
+    SUMMARY:
+    """
+    map_prompt_template = PromptTemplate(
+        template=map_prompt, input_variables=['text', 'objective'])
     
-scrape_website("Syed F Ahmad's GitHub", 'https://github.com/furquan-lp')
+    summary_chain = load_summarize_chain(
+        llm=llm,
+        chain_type='map_reduce',
+        map_prompt=map_prompt_template,
+        combine_prompt=map_prompt_template,
+        verbose=True
+    )
+
+    output = summary_chain.run(input_documents=docs, objective=objective)
+    return output
